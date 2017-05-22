71d134883f448006cb64fb088c863b7132ef1abc
bonanashelby <bonanashelby@gmail.com>
2017-05-22 15:03:42 -0700

    working on closing the client and server but not there yet

--- src/client.py
+++ src/client.py
@@ -9,7 +9,7 @@ send message passed as arg to the server, passed thru socket
 accumulate reply sent by server into string and when full reply recieved
 close socket and return message
 """
-    infos = socket.getaddrinfo('127.0.0.1', 5000)
+    infos = socket.getaddrinfo('127.0.0.1', 5001)
     stream_infos = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_infos[:3])
     client.connect(stream_infos[-1])
@@ -18,10 +18,10 @@ close socket and return message
     buffer_length = 8
     reply_complete = False
     while not reply_complete:
-        part = client.recv(buffer_length)
-        print(part.decode('utf8'))
-        if len(part) < buffer_length:
-            break
+            part = client.recv(buffer_length)
+            print(part.decode('utf8'))
+            if len(part) < buffer_length:
+                client.close()
 
 
 if __name__ == '__main__':
--- src/server.py
+++ src/server.py
@@ -10,19 +10,21 @@ def server():
     msg recieved & echoes the conn. shoul be close - but server must remain running and accepting connections
     """
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5000)
+    address = ('127.0.0.1', 5001)
     server.bind(address)
     server.listen(1)
     conn, addr = server.accept()
+    message = 'This is our server message.'
+    conn.sendall(message.encode('utf8'))
     buffer_length = 8
     message_complete = False
     while not message_complete:
-        part = conn.recv(buffer_length)
-        print(part.decode('utf8'))
-        if len(part) < buffer_length:
-            break
-    message = 'This is our server message.'
-    conn.sendall(message.encode('utf8'))
+            part += conn.recv(buffer_length).decode('utf8')
+            if len(part) < buffer_length:
+                break
+    conn.sendall(part.encode('utf8'))
+    conn.close()
+
 
 if __name__ == '__main__':
     server()
