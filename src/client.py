fdabbfeffd44162377c3e2adb7e41a80b3f49f4e
bonanashelby <bonanashelby@gmail.com>
2017-05-23 17:51:34 -0700

    our servers are echoing - now to tuesdays lab

--- src/client.py
+++ src/client.py
@@ -1,24 +1,27 @@
 """This is our client server."""
 import socket
-import sys
 
 
 def client(message):
     """Our client function for our sockets."""
-    infos = socket.getaddrinfo('127.0.0.1', 5000)
+    infos = socket.getaddrinfo('127.0.0.1', 5001)
     stream_infos = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_infos[:3])
     client.connect(stream_infos[-1])
-    message = 'This is our client message.'
     client.sendall(message.encode('utf8'))
     buffer_length = 8
     reply_complete = False
+    msg = b' '
     while not reply_complete:
         part = client.recv(buffer_length)
-        print(part.decode('utf8'))
+        msg += part
         if len(part) < buffer_length:
+            print(msg.decode('utf8'))
             break
 
 
 if __name__ == '__main__':
-    client(sys.argv[0])
+    """Our name main."""
+    import sys
+    message = sys.argv[1]
+    client(message)
--- src/server.py
+++ src/server.py
@@ -1,24 +1,35 @@
 """This is our server file."""
 import socket
+import sys
 
 
 def server():
     """Our server function for our sockets."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5000)
+    address = ('127.0.0.1', 5001)
     server.bind(address)
     server.listen(1)
-    conn, addr = server.accept()
-    message = 'This is our server message.'
-    conn.sendall(message.encode('utf8'))
-    buffer_length = 8
-    message_complete = False
-    while not message_complete:
-        part = conn.recv(buffer_length)
-        print(part.decode('utf8'))
-        if len(part) < buffer_length:
-            break
+    while True:
+        try:
+            conn, addr = server.accept()
+            buffer_length = 8
+            message_complete = False
+            msg = b' '
+            while not message_complete:
+                part = conn.recv(buffer_length)
+                msg += part
+                if len(part) < buffer_length:
+                    break
+
+            conn.sendall(msg)
+            conn.close()
+
+        except KeyboardInterrupt:
+            server.close()
+            print("Shutting down echo server. Bye, bye!")
+            sys.exit()
 
 
 if __name__ == '__main__':
+    print("Echo server is running\n")
     server()
