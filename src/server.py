fdf71d99a07002ef8977271769e602c24646900a
Casey O'Kane <okanecasey@gmail.com>
2017-05-26 13:36:57 -0700

    Working on step 3 assignment.

--- src/server.py
+++ src/server.py
@@ -2,11 +2,13 @@
 import socket
 import sys
 import os
+import mimetypes
+import io
 
 
 MEDIA_TYPES = [
-    'image/jpeg',
-    'image/png',
+    'images/jpg',
+    'images/png',
 ]
 
 
@@ -29,13 +31,21 @@ def server():
                 msg += part
                 if len(part) < buffer_length:
                     break
-            if msg[-3:] == 'EOF':
-                msg = msg[:-3]
-            print(msg.decode('utf8'))
-            response = parse_request(msg.decode('utf8'))
+            header_lines = msg.decode('utf8').split('\r\n')
+            split_header = header_lines[0].split()
+            if parse_request(split_header):
+                response = resolve_uri(split_header[1])
+            else:
+                response = response_error(split_header)
+            if len(response) % buffer_length == 0:
+                response += 'EOF'
+            print('Response:', response)
             conn.sendall(response.encode('utf8'))
         except KeyboardInterrupt:
             print("\nClosing HTTP server.")
+            conn.close()
+            server.close()
+            sys.exit()
             break
         except(SyntaxError, TypeError):
             response_error()
@@ -54,12 +64,12 @@ def response_error(error):
     """Create a 500 server error."""
     err_msg = 'HTTP/1.1 500 Internal Server Error\r\n'
     if len(error) < 3:
-        err_msg += 'HTTP Request requires a Method, URI, and a Protocol.\r\n'
+        err_msg += 'HTTP Request requires 3 items, a Method, URI, and a Protocol.\r\n'
     elif len(error) > 3:
-        err_msg += 'Unknown arguements passed into request.\r\n'
+        err_msg += 'Unknown arguements passed with request.\r\n'
     else:
         if error[0] != 'GET':
-            err_msg += 'This server only accepts GET requests \r\n'
+            err_msg += 'This server only accepts GET requests\r\n'
         if error[2] != 'HTTP/1.1':
             err_msg += 'Client must use HTTP/1.1\r\n'
     err_msg += '\r\n'
@@ -73,28 +83,55 @@ def response_file_not_found(error):
 
 def parse_request(header):
     """Parse request from user to see if valid."""
-    split_header = header.split()
-    if len(split_header) != 3:
-        response = response_error(split_header)
-    elif split_header[0] != 'GET' or split_header[2] != 'HTTP/1.1':
-        response = response_error(split_header)
-    else:
-        response = response_ok()
+    if len(header) != 3:
+        return False
+    elif header[0] != 'GET' or header[2] != 'HTTP/1.1':
+        return False
+    return True
+
+
+def resolve_uri(uri):
+    """File path to our webroot directory."""
+    root = '../webroot/'
+    print("file is", search_directory(root + uri))
+    retrieved_file = search_directory(root + uri)
+    file_type = mimetypes.guess_type(uri)[0]
+    print('File Type is: ', file_type)
+    try:
+        if os.path.exists(retrieved_file):
+            if file_type in MEDIA_TYPES:
+                f = io.open(retrieved_file, 'rb')
+                response = response_ok(file_type) + str(f.read())
+                f.close()
+            elif file_type == 'text/plain':
+                response = response_ok(file_type)
+                f = io.open(retrieved_file)
+                response += f.read()
+                f.close()
+            elif os.path.isdir(root + uri):
+                response = response_ok('directory') + \
+                    prepare_directory(root + uri)
+        else:
+            response = response_file_not_found(uri)
+    except IOError():
+        return response_file_not_found(uri)
     return response
 
 
-def resolve_uri(path_file):
-    """File path to our webroot directory."""
-    path_file = os.path.abspath('/webroot')
-    directory = os.listdir(path_file)
-    print('<ul>')
-    for item in directory:
-        print('<li>%s</li>') % item
-        fullpath = os.path.join(path_file, item)
-        if os.path.isdir(fullpath):
-            resolve_uri(fullpath)
-    print('</ul>')
-    print(resolve_uri(path_file))
+def prepare_directory(directory):
+    """Create html for files in directory."""
+    listing = os.listdir(directory)
+    response = '<!DOCTYPE html><html><head><title>' + \
+        directory + '</title></head><body><ul>'
+    for file in listing:
+        response += '<li>' + file + '</li>'
+    response += '</ul></body></html>'
+    return response
+
+
+def search_directory(uri):
+    """Search for files in webroot directory."""
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), uri)
 
 
 if __name__ == '__main__':
